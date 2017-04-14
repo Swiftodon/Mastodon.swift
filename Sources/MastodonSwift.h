2fd1d60effe46763072266a655333f2cbbc6b53b
Thomas Bonk <thomas@meandmymac.de>
2017-04-14 22:53:41 +0200

    recreated macOS target. together with the pull request to Moya-Gloss, both target should build now.

---
+++ Sources/MastodonSwift.h
@@ -0,0 +1,19 @@
+//
+//  MastodonSwift.h
+//  MastodonSwift
+//
+//  Created by Marcus Kida on 11/4/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MastodonSwift.
+FOUNDATION_EXPORT double MastodonSwiftVersionNumber;
+
+//! Project version string for MastodonSwift.
+FOUNDATION_EXPORT const unsigned char MastodonSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MastodonSwift/PublicHeader.h>
+
+
