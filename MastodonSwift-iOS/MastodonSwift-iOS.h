1f3bb96b4bd4dd28b23ea71e90e28f63b9446735
Marcus Kida <marcus@kida.io>
2017-04-16 10:15:03 +1000

    Reorganize Project Structure to match macOS target

---
+++ MastodonSwift-iOS/MastodonSwift-iOS.h
@@ -0,0 +1,19 @@
+//
+//  MastodonSwift-iOS.h
+//  MastodonSwift-iOS
+//
+//  Created by Marcus Kida on 16/4/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MastodonSwift-iOS.
+FOUNDATION_EXPORT double MastodonSwift_iOSVersionNumber;
+
+//! Project version string for MastodonSwift-iOS.
+FOUNDATION_EXPORT const unsigned char MastodonSwift_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MastodonSwift_iOS/PublicHeader.h>
+
+
