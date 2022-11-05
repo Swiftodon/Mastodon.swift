e3fbb000ccff67e70fcd99c49176782b67cee7b1
Thomas Bonk <thomas@meandmymac.de>
2022-11-05 12:59:47 +0100

    Implemented boost/unboost

---
+++ Sources/MastodonSwift/MastodonClient+statusActions.swift
@@ -0,0 +1,36 @@
+//
+//  MastodonClientAuthenticated+statusActions.swift
+//  
+//
+//  Created by Thomas Bonk on 05.11.22.
+//
+
+import Foundation
+
+public extension MastodonClientAuthenticated {
+    func boost(status: Status) async throws -> Status {
+        // TODO: Check whether the current user already boosted the status
+        let request = try Self.request(
+            for: baseURL,
+            target: Mastodon.Statuses.reblog(status.id),
+            withBearerToken: token
+        )
+        
+        let (data, _) = try await urlSession.data(for: request)
+        
+        return try JSONDecoder().decode(Status.self, from: data)
+    }
+    
+    func unboost(status: Status) async throws -> Status {
+        // TODO: Check whether the current user already boosted the status
+        let request = try Self.request(
+            for: baseURL,
+            target: Mastodon.Statuses.unreblog(status.id),
+            withBearerToken: token
+        )
+        
+        let (data, _) = try await urlSession.data(for: request)
+        
+        return try JSONDecoder().decode(Status.self, from: data)
+    }
+}
