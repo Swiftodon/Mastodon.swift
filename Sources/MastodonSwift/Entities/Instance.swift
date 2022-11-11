03481f2a9f90a0c195746e2ad335418f854fb6fe
Thomas Bonk <thomas@meandmymac.de>
2022-11-11 17:40:28 +0100

    added request for reading instance informations

--- Sources/MastodonSwift/Entities/Instance.swift
+++ Sources/MastodonSwift/Entities/Instance.swift
@@ -5,12 +5,14 @@ public struct Instance: Codable {
     public let title: String?
     public let description: String?
     public let email: String?
+    public let thumbnail: String?
     
     enum CodingKeys: CodingKey {
         case uri
         case title
         case description
         case email
+        case thumbnail
     }
     
     public init(from decoder: Decoder) throws {
@@ -19,5 +21,6 @@ public struct Instance: Codable {
         self.title = try? container.decodeIfPresent(String.self, forKey: .title)
         self.description = try? container.decodeIfPresent(String.self, forKey: .description)
         self.email = try? container.decodeIfPresent(String.self, forKey: .email)
+        self.thumbnail = try? container.decodeIfPresent(String.self, forKey: .thumbnail)
     }
 }
