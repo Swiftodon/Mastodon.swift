649f8bb742117442851fa3690a490dc976e55b08
Thomas Bonk <thomas@meandmymac.de>
2022-11-14 12:21:39 +0100

    optional URLs are handled as strings

--- Sources/MastodonSwift/Entities/Card.swift
+++ Sources/MastodonSwift/Entities/Card.swift
@@ -8,20 +8,20 @@ public struct Card: Codable {
         case rich = "rich"  // iframe OEmbed. Not currently accepted, so won't show up in practice.
     }
 
-    public let url: URL?
-    public let title: String?
-    public let description: String?
+    public let url: URL
+    public let title: String
+    public let description: String
     public let type: CardType?
 
     public let authorName: String?
-    public let authorUrl: URL?
+    public let authorUrl: String?
     public let providerName: String?
-    public let providerUrl: URL?
+    public let providerUrl: String?
     public let html: String?
-    public let width: String?
-    public let height: String?
-    public let image: URL?
-    public let embedUrl: URL?
+    public let width: Int?
+    public let height: Int?
+    public let image: String?
+    public let embedUrl: String?
     public let blurhash: String?
 
     private enum CodingKeys: String, CodingKey {
